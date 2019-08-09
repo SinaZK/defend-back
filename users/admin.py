06548d9f3156796daaee871df0ece43b02ab1e16
Sina ZandKarimi <sinazandkarimi@gmail.com>
2019-08-09 16:01:13 +0430

    Add new Member to admin

--- users/admin.py
+++ users/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import ugettext_lazy as _
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import Member
+from utils.admin import BaseUserAdmin
 
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = Member
-    list_display = ['id', 'email', 'username',]
+class MemberAdmin(BaseUserAdmin, DjangoUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
 
-admin.site.register(Member, CustomUserAdmin)
+        #(_('Chests'), {'fields': ('chest1_is_empty')}),
+
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('id', 'username', 'phone_number')
+    list_filter = ('is_staff', 'last_login', 'created')
+    search_fields = ('phone_number', 'id')
+    ordering = ('-date_joined',)
+
+admin.site.register(Member, MemberAdmin)
