24999ced5f9e9c461eabb89bf2d800bf11594a4b
Sina ZandKarimi <sinazandkarimi@gmail.com>
2019-08-10 13:18:33 +0430

    Implement base url for ftp

--- book/admin.py
+++ book/admin.py
@@ -6,14 +6,18 @@ from utils.admin import BaseAdmin
 
 
 class BookAdmin(BaseAdmin):
-    list_display = ('id', 'title', 'author', 'price', 'img_url',)
+    list_display = ('id', 'title', 'author', 'price', 'img', 'img_url', 'removed')
 
-    def img_url(self, obj):
+    def img(self, obj):
         if obj.image:
             return mark_safe("<img src={}>".format(obj.image.url))
         else:
             return "No Image"
     
-    img_url.short_description = 'Image url'
+    def img_url(self, obj):
+        return mark_safe("<a href={}>دانلود</>".format(obj.image_url))
+    
+    img.short_description = 'عکس'
+    img_url.short_description = 'دانلود عکس'
 
 admin.site.register(Book, BookAdmin)
