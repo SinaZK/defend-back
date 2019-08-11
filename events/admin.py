9af31cdfd70cc35dde0b0a63452d40e979f6b2e7
Sina ZandKarimi <sinazandkarimi@gmail.com>
2019-08-11 15:59:29 +0430

    Add Event to admin

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Event
+from utils.admin import BaseAdmin
+
+
+class EventAdmin(BaseAdmin):
+    list_display = ('id', 'img', 'jalali_date', 'title', 'created')
+    list_editable = ()
+
+    def img(self, obj):
+        if obj.image:
+            return mark_safe("<img width=50 height=50 src={}>".format(obj.image_url))
+        else:
+            return "No Image"
+    
+    def img_url(self, obj):
+        return mark_safe("<a href={}>دانلود</>".format(obj.image_url))
+    
+    img.short_description = 'عکس'
+    img_url.short_description = 'دانلود عکس'
+
+admin.site.register(Event, EventAdmin)
