f084a23848f4c95919a3eca6719055a5a2720a58
Sina ZandKarimi <sinazandkarimi@gmail.com>
2019-08-14 23:43:38 +0430

    Implement BookOrderSerializer validate method

--- book/serializers.py
+++ book/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.response import Response
 from .models import Book, BookOrder, BookShopItem
 
 class BookSerializer(serializers.ModelSerializer):
@@ -21,7 +22,15 @@ class BookOrderSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         order = BookOrder.objects.create()
         items = validated_data['items']
+
         for item in items:
             BookShopItem.objects.create(order=order, book=item['book'], quantity=item['quantity']) 
 
         return order
+
+    def validate(self, data):
+        items = data['items']
+        if len(items) == 0:
+            raise serializers.ValidationError("Shop items could not be empty")
+
+        return data
