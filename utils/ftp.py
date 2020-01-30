d72f4c9b36f3422e9b4fc46ffbe996e8b741947a
Sina ZandKarimi <sinazandkarimi@gmail.com>
2020-01-31 01:57:54 +0330

    Update frp base url

--- utils/ftp.py
+++ utils/ftp.py
@@ -2,7 +2,8 @@ import os
 from uuid import uuid4
 from django.utils.deconstruct import deconstructible
 
-FTP_BASE_URL = 'http://dl.geogeogeo.cloud/'
+#FTP_BASE_URL = 'http://dl.geogeogeo.cloud/'
+FTP_BASE_URL = 'http://dl.defenddefend.com/'
 FTP_PUBLIC_DIR = 'public_html/'
 
 @deconstructible
