92a721e48df9079eefa061bdc84851238732cc6e
Sina ZandKarimi <sinazandkarimi@gmail.com>
2020-01-31 16:00:34 +0330

    Better Idea admin

--- idea/admin.py
+++ idea/admin.py
@@ -5,7 +5,9 @@ from .models import Idea
 from utils.admin import BaseAdmin
 
 class IdeaAdmin(BaseAdmin):
-    list_display = ('id', 'title', 'state', 'state_text', 'user')
+    list_display = ('id', 'title', 'state', 'state_text', 'user', 'code')
     list_editable = ()
+    list_filter = ('state',)
+    search_fields = ('id', 'code', 'title')
 
 admin.site.register(Idea, IdeaAdmin)
---
+++ idea/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from .models import Idea
+
+class IdeaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Idea
+        fields = ('title', 'body', 'name', 'service_location', 'education_degree',
+        'education_field', 'phone_number', 'internet_address', 'physical_address', 'category',
+        'state', 'state_text', 'code')
+        read_only_fields = ('state', 'state_text', 'code')
