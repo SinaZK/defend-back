1dfdd66be1aed145f4e7cf9471efe93652a9a014
Sina ZandKarimi <sinazandkarimi@gmail.com>
2020-01-31 15:50:54 +0330

    Add Idea to admin

--- idea/admin.py
+++ idea/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Idea
+from utils.admin import BaseAdmin
+
+class IdeaAdmin(BaseAdmin):
+    list_display = ('id', 'title', 'state', 'state_text', 'user')
+    list_editable = ()
+
+admin.site.register(Idea, IdeaAdmin)
